eb6110862e81e307a1fe7f53fd417c8d77ce298a
Luis Carlos <luisscarlostec@gmail.com>
2020-04-24 05:29:40 -0300

    First commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '26fc7961e712fac401c42dc25884d95a',
+    expiresIn: '1d',
+  },
+};
---
+++ src/database/migrations/1587479103874-CreateAppointments.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+/**
+ * Migration que ao ser executado yarn typeorm migration:run
+ * cria as colunas na tabela ou yarn typeorm migration:revert para desfazer
+ */
+export default class CreateAppointments1587479103874
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('appointments');
+  }
+}
